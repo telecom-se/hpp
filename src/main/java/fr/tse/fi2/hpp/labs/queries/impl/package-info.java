492c0ecbaa3c6be2f0c7fc358fa5efbefcb6f401
Christophe Gravier <christophe.gravier@telecom-st-etienne.fr>
2016-02-23 14:12:07 +0100

    clean room master for 2015-2016

--- src/main/java/fr/tse/fi2/hpp/labs/queries/impl/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Implementations of the various queries
- * 
- * 
- * @author Julien
- *
- */
-package fr.tse.fi2.hpp.labs.queries.impl;
--- src/main/java/fr/tse/fi2/hpp/labs/queries/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * @author Julien
- *
- * Abstract classes that must be extended to process queries
- *
- */
-package fr.tse.fi2.hpp.labs.queries;
