d5a5e6c200237e1dc5fb2156f4d3040cf4d7625a
Julien Subercaze <julien.subercaze@univ-st-etienne.fr>
2019-04-26 12:06:38 +0200

    Update Consumer.java

--- lab2/code/src/main/java/fr/tse/fise2/info4/lab10/exercise2/Consumer.java
+++ lab2/code/src/main/java/fr/tse/fise2/info4/lab10/exercise2/Consumer.java
@@ -6,10 +6,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * TODO Modify this class so that:
  * <ul>
- * <li>It counts the number of words in an application wide
- * {@link AtomicInteger} shared among the {@link Consumer}</li>
- * <li>Counts the occurrence of each word using an application wide
- * {@link ConcurrentHashMap} shared among the {@link Consumer}</li>
+ * <li>It counts the number of words</li>
  * </ul>
  * 
  *
