fe8c95d483545e4b3b756117ae40170b48b3fb24
Guillaume MULLER <guillaume.muller@univ-st-etienne.fr>
2022-02-25 12:36:28 +0100

    reAdded 2016-2017 MT Lab

---
+++ lab4-MultiThreading/code/src/test/java/fr/tse/fise2/info4/lab10/exercise2/TestExercise2.java
@@ -0,0 +1,18 @@
+package fr.tse.fise2.info4.lab10.exercise2;
+
+import org.junit.Test;
+
+public class TestExercise2 {
+
+	/**
+	 * TODO Write a test that:
+	 * <li>Creates a {@link Producer}, n {@link Consumer}s</li>
+	 * <li>Asserts that the number of words produced by the {@link Producer} is the same as the one obtained from the {@link Consumer}s</li>
+	 */
+	@Test
+	public void test() {
+
+		
+	}
+
+}
